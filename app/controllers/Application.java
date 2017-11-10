a50cf72e90909e750d17fad50a8e5d21bb365bab
Jakob <jakmanne@gmail.com>
2017-11-10 15:58:05 +0100

    Tester endring

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,19 +1,29 @@
 package controllers;
 
 import play.*;
+import play.data.Form;
 import play.mvc.*;
+import Models.Bruker;
 
 import views.html.*;
 
 public class Application extends Controller {
 
+    //GET metode som lases når localhost9000 kalles. Definert i Routes mappen.
     public static Result index() {
         return ok(index.render("Your new application is ready."));
     }
 
-    //For å kjøre denne i nettleseren skriv inn localhost9000/Hello/Jakob
-    public static Result hello(String name) {
-        return ok("Hello " + name + "!");
-    }
+     //POST metode som kalles fra viewet når jeg trykket på en knapp. Sender en redirect til viewet.
+    //Return brukes når man vil sende noe til en tilhørende controller. mens redirect er for å sende til et view et annet sted. (som regel).
+   public static Result addBar(){
+
+       //Lager et bruker objekt fra requesten. DVS Når vi får inn JPA så oppretter vi en auksjon/bruker her.
+       Bruker bruker = Form.form(Bruker.class).bindFromRequest().get();
+       System.out.println(bruker.getNavn());
+       return redirect(routes.Application.index());
+   }
+
+
 
 }
